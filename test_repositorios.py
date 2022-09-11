9def2c366bb2ff80484fbcf92e495cc8abb2d377
Julen Ray <112992813+ray-industries@users.noreply.github.com>
2022-09-11 07:04:13 -0300

    Update test_repositorios.py

--- test_repositorios.py
+++ test_repositorios.py
@@ -1,1 +1,20 @@
-print("huwhduwhduwdhwhduwdhu")
+import socket
+import pyttsx3
+
+engine = pyttsx3.init()
+soke = socket.socket()
+
+soke.bind(("192.168.137.75", 8000))
+
+soke.listen(5)
+
+while True:
+    conexion, addrs = soke.accept()
+    engine.say("Conexion realizada en", str(addrs))
+    engine.runAndWait()
+    mensaje = "Saludo desde el servidor"
+    conexion.send(mensaje.encode("utf-8"))
+    recibido = conexion.recv(1024)
+    print(recibido)
+    print(type(recibido))
+    conexion.close()
